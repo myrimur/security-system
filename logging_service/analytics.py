0984dfa9f523ff0e7333a045969857656156b050
myrimur <m.t.sobko@gmail.com>
2023-05-26 20:55:04 +0300

    Clean up analytics.py

--- logging_service/analytics.py
+++ logging_service/analytics.py
@@ -12,31 +12,5 @@ while True:
     mapped_df = log_df.select(col("location"), col("person_id")).withColumn("appearance_count", lit(1))
     reduced_df = mapped_df.groupBy("location", "person_id").agg(sum("appearance_count").alias("total_appearances"))
     reduced_df.repartition(1).write.json("/opt/app/results", mode="overwrite")
-    # reduced_df.repartition(1).write.option("multiLine", "true").json("/opt/app/results", mode="overwrite")
-    # sleep(5 * 60)
     sleep(60)
 
-# log_df = spark.read.format("org.apache.spark.sql.cassandra").options(table="by_person_id", keyspace="appearances").load()
-# mapped_df = log_df.select(col("location"), col("person_id")).withColumn("appearance_count", lit(1))
-# reduced_df = mapped_df.groupBy("location", "person_id").agg(sum("appearance_count").alias("total_appearances"))
-# reduced_df.repartition(1).write.json("/opt/app/results", mode="overwrite")
-
-# from pyspark.sql import SparkSession
-# spark = SparkSession.builder.appName('SimpleSparkProject').getOrCreate()
-#
-# columns = ["name","languagesAtSchool","currentState"]
-# data = [("James,,Smith",["Java","Scala","C++"],"CA"), \
-#     ("Michael,Rose,",["Spark","Java","C++"],"NJ"), \
-#     ("Robert,,Williams",["CSharp","VB"],"NV")]
-#
-# df = spark.createDataFrame(data=data,schema=columns)
-# df.printSchema()
-# df.show(truncate=False)
-# df.write.json("/opt/app/results", mode="overwrite")
-
-# from pyspark.sql import SparkSession
-#
-# spark = SparkSession.builder.appName('LineCounter').getOrCreate()
-# df = spark.read.option("header", "true").csv('/opt/app/PS_20174392719_1491204439457_log.csv', header=True)
-# print("Number of rows: ", df.count())
-# df.repartition(1).write.json("/opt/app/results", mode="overwrite")
